8afa3b5351152fc4a7f6ba990d5b30897e38c68f
deMGoncalves / <cleber.engineer@hotmail.com>
2023-12-12 12:18:47 +0000

    refactor(text): compondo o texto apenas com suas dependencias

--- src/elements/text/text.js
+++ src/elements/text/text.js
@@ -1,16 +1,19 @@
-import * as p from '@standard/polished'
+import align from './align'
+import color from './color'
+import family from './family'
+import size from './size'
 import styled from '@standard/styled'
+import weight from './weight'
 
 const text = new Proxy({}, {
   get (_target, key) {
     return styled[key]`
-      color: ${p.color};
-      font-family: ${p.family};
-      font-size: ${p.size};
-      font-weight: ${p.weight};
-      letter-spacing: .618px;
-      line-height: var(--line-height-md);
-      text-align: ${p.align};
+      color: ${color};
+      font-family: ${family};
+      font-size: ${size};
+      font-weight: ${weight};
+      line-height: var(--line-height-xl);
+      text-align: ${align};
     `
   }
 })
