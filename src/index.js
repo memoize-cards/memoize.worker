5fbcb94e983535fcb2ebffdcbc2ea4106fe93c90
deMGoncalves / <cleber.engineer@hotmail.com>
2023-12-15 15:28:19 +0000

    style: padronizacao do codigo

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import asset from '@standard/asset'
 import router from '@standard/router'
 
 export default {
-  async fetch (request, ...args) {
+  async fetch (request) {
     return asset.is(request)
-      ? await asset.handle(request, ...args)
-      : await router.handle(request, ...args)
+      ? await asset.handle(...arguments)
+      : await router.handle(...arguments)
   }
 }
